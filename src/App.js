a74abdb00dae200c6c047e56f90fc772f2979afe
noveski99 <noveski99@gmail.com>
2024-02-20 03:14:45 +0100

    added icon

--- src/App.js
+++ src/App.js
@@ -19,27 +19,20 @@ import { DiPostgresql } from "react-icons/di";
 import { FaToggleOff } from "react-icons/fa";
 import { FaToggleOn } from "react-icons/fa";
 import { MdOutlineSubdirectoryArrowRight } from "react-icons/md";
-
-
-
-
+import { FaGitAlt } from "react-icons/fa";
 function App() {
   const [isToggled, setToggled] = useState(false)
-
   const handleToggle = () => {
     setToggled(!isToggled);
   }
-
   return (
     <div className="App">
       <nav className='header'>
         {isToggled ? <FaToggleOn title='Turn off' size={30} onClick={handleToggle} className={`toggle-button ${isToggled && 'toggle-button-on'}`} /> :
           <FaToggleOff title='Highlight skills' onClick={handleToggle} size={30} className='toggle-button'
           />}
-
         <ul>
-
-          <li title='Profile'><MdAccountCircle size={30}/></li>
+          <li title='Profile'><MdAccountCircle size={30} /></li>
           <li title='E-mail'><a href='mailto:noveski99@gmail.com?Subject=YourSubject'><IoIosMail size={30} /></a></li>
           <li title='Github'><a href='https://github.com/noveski99' target="_blank" rel="noreferrer"><FaGithub size={30} /></a></li>
         </ul>
@@ -52,31 +45,23 @@ function App() {
               <p id='second-p'>Boban</p>
               <p id='third-p'>FCSE student, passionate about software development, who likes to craft well founded frontend products with great user experience.</p>
             </div>
-
-
           </div>
- 
-
         </aside>
 
         <aside className='section-right'>
           <h1>PROJECTS</h1>
-
           <section className='section-right-inner'>
             <p><a href='https://todo-appproject1.netlify.app/' target="_blank" rel="noreferrer">TodoPal</a></p>
             <p><a href='https://weather-appproject1.netlify.app/' target="_blank" rel="noreferrer">Waily</a></p>
             <p>Project 3</p>
           </section>
-
         </aside>
         <section className='section-bottom'>
-
           <div className='section-bottom-left'> <p><MdOutlineSubdirectoryArrowRight />SKILLS</p></div>
-
           <div className={`square ${isToggled && 'on'}`}>
             <div title='HTML5' className='inner-square'><FaHtml5 size={30} /></div>
             <div title='CSS3' className='inner-square'><FaCss3Alt size={30} /></div>
-            <div title='JAVASRIPT' className='inner-square'><DiJavascript size={30} /></div>
+            <div title='JAVASCRIPT' className='inner-square'><DiJavascript size={30} /></div>
             <div title='TYPESCRIPT' className='inner-square'><SiTypescript size={30} /></div>
             <div title='REACT' className='inner-square'><FaReact size={30} /></div>
             <div title='JQUERY' className='inner-square'><DiJqueryLogo size={30} /></div>
@@ -84,20 +69,14 @@ function App() {
             <div title='C++' className='inner-square'><TbBrandCpp size={30} /></div>
             <div title='POSTMAN' className='inner-square'><SiPostman size={30} /></div>
             <div title='POSTGRESQL' className='inner-square'><DiPostgresql size={30} /></div>
+            <div title='GIT' className='inner-square'> <FaGitAlt size={30} /></div>
           </div>
         </section>
-
       </main>
-
-
       <footer className='footer'>
         <p>© 2024 noveski99</p>
       </footer>
     </div>
-
-
   );
 }
-
 export default App;
- 
